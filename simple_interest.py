c1741a440481e683714b8bf8a656c78d125fdc5e
Frank Lamar <lamar-frankie@users.noreply.github.com>
2020-08-29 00:08:08 -0400

    Create simple_interest.py

---
+++ simple_interest.py
@@ -0,0 +1,18 @@
+#In this exercise you will create a function that will show the user the total call of a simple interest loan. 
+#The formula for simple interest is Interest = principal x rate x time
+
+
+
+#Prompt the user to enter their principal
+
+
+#Prompt the user to enter their interest rate
+
+
+#Prompt the user to enter the time of their loan in years
+
+
+#calculate the to total interest
+
+
+#Calculate the total amount the user would pay
