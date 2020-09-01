e60b754730dd969a557c128db4f8cf7689eb0886
Frank Lamar <lamar-frankie@users.noreply.github.com>
2020-09-01 18:30:57 -0400

    Update weight_on_the_moon.py

--- weight_on_the_moon.py
+++ weight_on_the_moon.py
@@ -1,12 +1,16 @@
 #This script is used to calculate your weight on the moon. 
 
 #Create a constant and set it equal to the moon weight differential of 1.622
+MOON_WEIGHT_DIFFERENTIAL = 1.22
 
 
 #Ask the user to enter their weight
+user_weight = input("Enter your weight: ")
 
 
 #Calculate the users weight on the moon (Moon weight = Earth weight * moon weight differential)
-
-
-#Print this result to the user
+def calculate_weight_on_moon(weight):
+  moon_weight = weight * MOON_WEIGHT_DIFFERENTIAL
+  print(moon_weight)
+  
+  
