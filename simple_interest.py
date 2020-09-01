1755de35efd4fe0d3b7bc485df364c0a006c71b1
newplanetww <wadesexton@gmail.com>
2020-08-31 21:48:41 -0400

    Update simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,9 +1,45 @@
 #In this exercise you will create a function that will show the user the total call of a simple interest loan. 
 #The formula for simple interest is Interest = principal x rate x time
+p = principal
+t = time
+r = rate
+
+print("Enter the principal in dollars:")
+principal = input(p)
+print("Your principal is"+ principal)
+
+print("Enter the principal in dollars:")
+time = input(t)
+print("Your time is"+ time)
+
+print("Enter the principal in dollars:")
+rate = input(r)
+print("Your rate is"+ rate)
+
+p= input('enter value for p')
+t= input('enter value for t')
+r= input('enter value for r')
+
+print(int(p)*int(r)*int(t))
 
 
+
+
+#def simple_interest(p,t,r): 
+ #   print('The principal is', p) 
+  #  print('The time period is', t) 
+  #  print('The rate of interest is',r) 
+      
+    #si = (p * t * r)/100
+      
+    #print('The Simple Interest is', si) 
+    #return si 
+      
+# Driver code 
+#simple_interest(8, 6, 8) 
 
 #Prompt the user to enter their principal
+
 
 
 #Prompt the user to enter their interest rate
@@ -16,3 +52,10 @@
 
 
 #Calculate the total amount the user would pay
+
+
+
+
+
+
+
